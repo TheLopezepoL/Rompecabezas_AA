cef856efd1d243bd0179b4cd210a9a314bd04857
Sebastian <thelopezepol@estudiantec.cr>
2021-09-29 04:47:01 -0600

    main mas bonito

--- Code/src/main/Main.java
+++ Code/src/main/Main.java
@@ -17,32 +17,23 @@ public class Main {
         System.out.println("\nDesordenar puzzle");
         obj.printPuzzle();
 
-        //System.out.println("\nBrute force");
-        //obj.bruteForce(0,0);
-        //obj.printBruteForce();
+        System.out.println("\nBrute force");
+        obj.bruteForce(0,0);
+        obj.printBruteForce();
+
+        obj.shufflePuzzle();
+        System.out.println("\nDesordenar puzzle");
+        obj.printPuzzle();
 
         System.out.println("\nAlgoritmo Probabilistico");
-        PuzzleResolver asd = new PuzzleResolver();
-        asd.fillUnsignedPieces(obj);
-        if (asd.solvePuzzle())
-            asd.getPuzzle().printPuzzle();
-        else
-            System.out.println("No se pudo resolver");
-
-        System.out.println("\nSegundo Intento");
-        PuzzleResolver asd2 = new PuzzleResolver();
-        asd2.fillUnsignedPieces(obj);
-        if (asd2.solvePuzzle())
-            asd2.getPuzzle().printPuzzle();
-        else
-            System.out.println("No se pudo resolver");
-
-        System.out.println("\nTercer Intento");
-        PuzzleResolver asd3 = new PuzzleResolver();
-        asd3.fillUnsignedPieces(obj);
-        if (asd3.solvePuzzle())
-            asd3.getPuzzle().printPuzzle();
-        else
-            System.out.println("No se pudo resolver");
+        for (int intento = 1; intento < 4; intento++) {
+            System.out.println("Intento: " + intento);
+            PuzzleResolver asd = new PuzzleResolver();
+            asd.fillUnsignedPieces(obj);
+            if (asd.solvePuzzle())
+                asd.getPuzzle().printPuzzle();
+            else
+                System.out.println("No se pudo resolver");
+        }
     }
 }
