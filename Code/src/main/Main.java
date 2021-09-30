48a699c0d55bbcf3454fdc0ac3e02a396cc73fad
breiner2000 <61507252+breiner2000@users.noreply.github.com>
2021-09-30 15:39:47 -0600

    assignments & comparisons
    set assignments and comparisons on bruteForce/fastForward algorithm

--- Code/src/main/Main.java
+++ Code/src/main/Main.java
@@ -18,24 +18,35 @@ public class Main {
         obj.printPuzzle();
 
         System.out.println("\nBrute force");
+        long startBruteForce = System.currentTimeMillis(); //start execution time
         obj.bruteForce(0,0);
+        long stopBruteForce = System.currentTimeMillis();  //stop execution time
+        double bruteForceTime = (double) ((stopBruteForce - startBruteForce));  //execution time
         obj.printBruteForce();
+        obj.printAsigCompBF();
+        System.out.println("\n BruteForce: "+ bruteForceTime +" milisegundos");
 
         obj.shufflePuzzle();
         System.out.println("\nDesordenar puzzle");
         obj.printPuzzle();
 
         System.out.println("\nAlgoritmo Probabilistico");
+        long startFastForward = System.currentTimeMillis(); //start execution time
         for (int intento = 1; intento < 4; intento++) {
             System.out.println("Intento: " + intento);
             PuzzleResolver asd = new PuzzleResolver();
             asd.fillUnsignedPieces(obj);
             if (asd.solvePuzzle()) {
                 asd.getPuzzle().printPuzzle();
+                asd.printAsigCompFF();
                 break;
             }
             else
                 System.out.println("No se pudo resolver");
         }
+        long stopFastForward = System.currentTimeMillis();  //stop execution time
+        double fastForwardTime = (double) ((stopFastForward - startFastForward));  //execution time
+        System.out.println("\n Fast Forward: "+ fastForwardTime +" milisegundos");
+
     }
 }
